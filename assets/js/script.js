86113bb5c3777ff024d52a87fcf98b9f4e242f62
Levi ikechukwu <leviikechukwu12@gmail.com>
2024-08-18 07:19:07 -0700

    First commit

---
+++ assets/js/script.js
@@ -0,0 +1,30 @@
+var mobile = document.getElementById("mobile");
+var open = document.getElementById("open");
+var close = document.getElementById("close");
+
+
+
+open.addEventListener("click", function(){
+  mobile.style.display = "flex"
+  
+})
+close.addEventListener("click", function(){
+  mobile.style.display = "none"
+})
+
+
+
+var swiper = new Swiper(".mySwiper", {
+  cssMode: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  mousewheel: true,
+  keyboard: true,
+});
+
+
